3009001f226d4d240e7941eedc8349035ef5bb0c
binarylei <binarylei@qq.com>
2019-11-04 22:40:26 +0800

    quartz spring-boot

---
+++ crontab/crontab-quartz-springboot/src/main/java/quartz/springboot/QuartzApp.java
@@ -0,0 +1,14 @@
+package quartz.springboot;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan("com.gupaoedu.demo.mapper")
+public class QuartzApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuartzApp.class, args);
+	}
+}
